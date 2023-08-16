df1582775222de636c45795d98a25f2a835086f4
themagicianking <tmoonder@gmail.com>
2023-08-16 16:32:14 -0400

    Initial setup.

---
+++ main.rb
@@ -0,0 +1,36 @@
+class LinkedList
+  def append(value)
+  end
+
+  def prepend(value)
+  end
+
+  def size
+  end
+
+  def head
+  end
+
+  def tail
+  end
+
+  def at(index)
+  end
+
+  def pop
+  end
+
+  def contains?(value)
+  end
+
+  def find(value)
+  end
+
+  def to_s
+  end
+end
+
+class Node
+  def value
+  end
+end

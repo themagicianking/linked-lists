1cd48e1b44f464d2086d13db98cbf8a27f0c8505
themagicianking <tmoonder@gmail.com>
2023-08-20 21:30:17 -0400

    Created a node class that will store values and that will be added to a linked list when created.

--- main.rb
+++ main.rb
@@ -1,5 +1,17 @@
 class LinkedList
+  attr_accessor :node, :value
+
+  def initialize
+    @node = Node.new
+    @node.value()
+  end
+
   def append(value)
+    if @node.node.nil?
+      @node.value(value)
+    else
+      # ???
+    end
   end
 
   def prepend(value)
@@ -31,6 +43,16 @@ class LinkedList
 end
 
 class Node
-  def value
+  attr_accessor :node, :value
+
+  def value(node=nil)
+    @node = node
   end
 end
+
+list = LinkedList.new
+
+list.append(3)
+list.append(5)
+
+pp list
